bd57764d50860913b69864c0df299a12a1f5c1ee
phocsoccute1412 <khanhnguyenkatee1412@gmail.com>
2022-04-16 01:13:49 +0700

    Food Web

---
+++ js/testPEWeb.js
@@ -0,0 +1,15 @@
+function changeImage1() {
+    document.getElementById('anh1').src = "images/miquang4.jpg";
+}
+
+function returnImage1() {
+    document.getElementById('anh1').src = "images/miquang.jpg";
+}
+
+function changeImage2() {
+    document.getElementById('anh2').src = "images/chagio.jpg";
+}
+
+function returnImage2() {
+    document.getElementById('anh2').src = "images/chagio2.jpg";
+}
